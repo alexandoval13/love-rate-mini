78355379b78c4b3cdad7dade1f53521aacc01e39
Alexandra Sandoval <alexandoval13@gmail.com>
2025-05-22 13:24:52 -0700

    Writes axios TMDB API get request

---
+++ app/utils/api.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const api = axios.create({
+  baseURL: BASE_URL,
+  params: {
+    api_key: API_KEY,
+    language: 'en-US',
+  },
+  timeout: 5000,
+});
+
+export interface MovieSummary {
+  id: number;
+  title: string;
+  release_date: string;
+  poster_path: string;
+  overview: string;
+  vote_average: number;
+}
+
+export async function searchMovies(query: string): Promise<MovieSummary[]> {
+  const res = await api.get('/search/movie', {
+    params: { query },
+  });
+  console.log({ res: res.data.results });
+  return res.data.results as MovieSummary[];
+}
