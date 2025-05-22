601e110611282c358f43a09bd84e0a057e6f0172
Alexandra Sandoval <alexandoval13@gmail.com>
2025-05-21 19:12:22 -0700

    Remove test route and component

--- app/test/test.tsx
+++
@@ -1,9 +0,0 @@
-export function TestComponent() {
-  return (
-    <main className="flex items-center justify-center pt-16 pb-4">
-      <div>
-        <h1>This is a test component</h1>
-      </div>
-    </main>
-  );
-}
