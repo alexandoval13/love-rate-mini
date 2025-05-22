12844a8abea2a8ff4778f446aba96f17c0d1404c
Alexandra Sandoval <alexandoval13@gmail.com>
2025-05-21 19:13:38 -0700

    Add and config vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+/// <reference types="vitest" />
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
 });
