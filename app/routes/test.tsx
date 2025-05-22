bc708aeda1320e64b1a0cab293b712c032333466
Alexandra Sandoval <alexandoval13@gmail.com>
2025-05-21 17:29:01 -0700

    Create a test route and component

---
+++ app/routes/test.tsx
@@ -0,0 +1,13 @@
+import { TestComponent } from '~/test/test';
+// import type { Route } from './+types/test';
+
+// export function meta({}: Route.MetaArgs) {
+//   return [
+//     { title: 'Love/Rate (mini)' },
+//     { name: 'description', content: 'With love, your movies' },
+//   ];
+// }
+
+export default function Test() {
+  return <TestComponent />;
+}
---
+++ app/test/test.tsx
@@ -0,0 +1,9 @@
+export function TestComponent() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <div>
+        <h1>This is a test component</h1>
+      </div>
+    </main>
+  );
+}
